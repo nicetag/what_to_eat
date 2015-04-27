bf96a85b83319a6aaec6f0db46f2e06b92622fb1
ccQpein <ccq777@gmail.com>
2015-04-27 15:58:41 +0800

    random select store done

--- StoreData.py
+++ StoreData.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 # -*- coding=utf-8 -*-
 
+import click
+
 class store_init(object):
     """basic store class"""
     def __init__(self):
@@ -12,3 +14,8 @@ class store_init(object):
     def address(self, argv):
         self.address = argv
 
+store_list = ["老家", "麦当劳", "拉面", "麻辣烫"]
+
+def add(s):
+    global store_list
+    store_list.append(s)
--- main.py
+++ main.py
@@ -2,5 +2,30 @@
 # -*- coding=utf-8 -*-
 
 import __init__
+import random
 import click
+import StoreData
 
+store_list = StoreData.store_list
+
+@click.command()
+@click.option('--dec', prompt='to eat(1) or eaten(2)', help='Choise the logical module')
+def choice_module(dec):
+    dec = int(dec)
+    if dec == 1:
+        i = random.randint(0, len(StoreData.store_list))
+        click.echo(StoreData.store_list[i])
+
+    if dec == 2:
+        slist = input_store_name()
+        print(slist)
+
+@click.command()
+@click.option('--store_name', prompt='what is store name?', help='enter store name')
+def input_store_name(store_name):
+    a = StoreData.store_init()
+    a.name(store_name)
+    StoreData.add(a.name)
+
+if __name__ == '__main__':
+    choice_module()
